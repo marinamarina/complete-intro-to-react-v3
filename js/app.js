73a7196648d8ec8fb99dbaac551d15315dc48d70
Marina Shchukina <Marina.Shchukina@4finance.com>
2017-10-09 12:45:32 +0200

    Add basic webpack

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom' 
 
-const MyTitle = function(props) {
-  return React.createElement('div', null, 
-    React.createElement('h1', {style: {color: props.color}}, props.title)
-  )
-}
-const MyFirstComponent = function () {
-  return React.createElement('div', null, 
-    [React.createElement(MyTitle, {title: 'Helloo', key: 1, color: 'red' }),
-    React.createElement(MyTitle, {title: 'Good bye', key: 2, color: 'orange' })]
-  )
-}
-
-render(
-  React.createElement(MyFirstComponent), 
-  document.getElementById('app')
+const App = () => (
+  <h1>Hey</h1>
 )
+
+render(React.createElement(App), document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './js/app.js',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',          
+          include: path.join(__dirname, '/js'),
+        }
+      ],
+  }
+}
