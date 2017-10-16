a98c7c46836efe3328e5a67db1e025db34c7f055
Marina Shchukina <Marina.Shchukina@4finance.com>
2017-10-16 11:48:00 +0200

    Add devserver

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { render } from 'react-dom' 
 
 const App = () => (
-  <h1>Hey</h1>
+  <div className='app'>
+    <div className='landing'>
+      <h1>search app</h1>
+      <input type="text" placeholder="Search"/>
+      <a href="/">or Browse all</a>
+    </div>
+  </div>
 )
 
 render(React.createElement(App), document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,20 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'app.bundle.js'
   },
+  devServer: {
+    publicPath: 'bundle'
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   },
   module: {
     rules: [
+        {
+          enforce: 'pre',
+          test: /\.jsx?$/,
+          loader: 'eslint-loader',
+          include: path.join(__dirname, '/js')          
+        },
         {
           test: /\.jsx?$/,
           loader: 'babel-loader',          
